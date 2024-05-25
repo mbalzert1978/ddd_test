1ca3434912bf755478358ea4f9d9794779da27f9
Markus Iorio <mbalzert@gmail.com>
2024-05-25 21:40:05 +0200

    error type

---
+++ src/Domain/Primitives/Error.cs
@@ -0,0 +1,7 @@
+namespace Domain;
+
+public sealed record Error(string Code, string? Description = null)
+{
+    public static readonly Error None = new(string.Empty,
+                                            string.Empty);
+}
