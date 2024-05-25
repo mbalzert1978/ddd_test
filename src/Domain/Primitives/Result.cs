2ab6f94916bce719c4b64ced1c033a9c75176c58
Markus Iorio <mbalzert@gmail.com>
2024-05-25 21:39:48 +0200

    result type

---
+++ src/Domain/Primitives/Result.cs
@@ -0,0 +1,34 @@
+﻿namespace Domain;
+
+public class Result
+{
+    private const string InvalidResult = "Result must be either success or failure, but not both.";
+
+    private Result(bool isSuccess, Error error)
+    {
+        if (isSuccess
+            && error != Error.None
+            || !isSuccess
+            && error == Error.None)
+        {
+            throw new ArgumentException(InvalidResult,
+                                        nameof(error));
+        }
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+
+    public static Result Success() => new(true, Error.None);
+    public static Result Failure(Error error) => new(false, error);
+}
+
+public sealed record Error(string Code, string? Description = null)
+{
+    public static readonly Error None = new(string.Empty,
+                                            string.Empty);
+}
