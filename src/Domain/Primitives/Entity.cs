9aeb1de2fd0dcf2501e302c3f1dfcaacdf72344b
Markus Iorio <mbalzert@gmail.com>
2024-11-16 18:10:35 +0100

    fmt

--- src/Domain/Primitives/Entity.cs
+++ src/Domain/Primitives/Entity.cs
@@ -3,16 +3,14 @@ namespace Domain.Primitives;
 public abstract class Entity(Guid id) : IEquatable<Entity> {
     protected Guid Id { get; } = id;
 
-public static bool operator ==(Entity? left, Entity? right) => Equals(left, right);
+    public static bool operator ==(Entity? left, Entity? right) => Equals(left, right);
 
-public static bool operator !=(Entity? left, Entity? right) => !Equals(left, right);
+    public static bool operator !=(Entity? left, Entity? right) => !Equals(left, right);
 
-public override bool Equals(object? obj) => Equals(obj as Entity);
+    public override bool Equals(object? obj) => Equals(obj as Entity);
 
-public bool Equals(Entity? other) =>
-    other is not null &&
-    GetType() == other.GetType() &&
-    Id == other.Id;
+    public bool Equals(Entity? other) =>
+        other is not null && GetType() == other.GetType() && Id == other.Id;
 
-public override int GetHashCode() => HashCode.Combine(GetType(), Id) * 42;
+    public override int GetHashCode() => HashCode.Combine(GetType(), Id) * 42;
 }
